2fbcd84330af1ba449a80f6b76f682a91bf51227
weilinfox <weilin.king@qq.com>
2022-10-08 11:18:04 +0800

    go statically build

--- scripts/tasks.sh
+++ scripts/tasks.sh
@@ -5,15 +5,15 @@ export GOPATH=$HOME/go
 DIR_SHITAMA=$(pwd)
 
 function build_holder() {
-    go build -o ./build/holder/holder ./holder/
+    CGO_ENABLED=0 go build -a -ldflags '-extldflags "-static"' -o ./build/holder/holder ./holder/
 }
 
 function build_shard() {
-    go build -o ./build/shard/shard ./shard/
+    CGO_ENABLED=0 go build -a -ldflags '-extldflags "-static"' -o ./build/shard/shard ./shard/
 }
 
 function build_client() {
-    go build -o ./build/client/client ./client/
+    CGO_ENABLED=0 go build -a -ldflags '-extldflags "-static"' -o ./build/client/client ./client/
 }
 
 function build_all() {
